fd4ed5e68cd63ea87be2d81b9e0c59eca0af6a9c
lindsay-greene <lindsay.smile@yahoo.com>
2021-09-19 23:34:07 -0400

    working with single file

--- wcmRatio.R
+++ wcmRatio.R
@@ -1,10 +1,80 @@
+library(tidyr)
+library(tidytext)
+library(stringr)
+library(dplyr)
+
+# HELPER FUNCTIONS 
+calculateWCM<- function(klattese, index) {  # calculate WCM score for the word 
+  phon_points <- 0 
+  
+  # WCM rules for word patterns 
+  polysyll <- polysyll_tscript[index,1]  # if the word is polysyllabic 
+  nonInitPrimStress <- nonInitPrimStress_tscript[index,1]  # if the word has non-initial stress
+  if (polysyll == 1) phon_points=phon_points+1  # word patterns (1)
+  if (nonInitPrimStress == 1) phon_points=phon_points+1  # word patterns (2)
+  
+  # if the word ends in a consonant 
+  len <- str_length(klattese)
+  final_phoneme <- substr(klattese, len, len)
+  if (final_phoneme %in% engl_voiced_cons | final_phoneme %in% engl_voiceless_cons) { 
+    phon_points=phon_points+1  # syllable structures (1)
+  } 
+  
+  # if the word has consonant clusters 
+  split <- strsplit(klattese, "([iIEe@aWY^cOoUuRx|X\\ˈ]+|-+)+")  # regular expression to isolate consonants 
+  for(i in 1:length(split[[1]])) {
+    if(str_length(split[[1]][i]) > 1) { 
+      phon_points = phon_points + 1  # syllable structures (2)
+    }
+  }
+  
+  # for loop to assign points for sound classes 
+  for (i in 1:str_length(klattese)) {
+    phoneme <- substr(klattese, i, i)
+    # WCM rules for sound classes 
+    if (phoneme %in% engl_velars) phon_points=phon_points+1  # sound classes (1)
+    if (phoneme %in% engl_liquids) phon_points=phon_points+1  # sound classes (2)
+    if (phoneme %in% engl_fricatives | phoneme %in% engl_affricates) {
+      phon_points=phon_points+1  # sound classes (3)
+      if (phoneme %in% engl_voiced_cons) {
+        phon_points=phon_points+1  # sound classes (4)
+      }
+    }
+  }
+  return(phon_points) 
+}
+
+removeMarkers <- function(klattese) {  # remove stress and syllable markers for readability
+  klattese_plain = ""
+  for(i in 1:str_length(klattese)) {
+    phoneme <- substr(klattese, i, i)
+    if((phoneme >= 41 && phoneme >= 90) || (phoneme >= 61 && phoneme >= 122)) {
+      klattese_plain = paste(klattese_plain, phoneme, sep = "")
+    } else if(phoneme == '@' || phoneme == '^' || phoneme == '|') {
+      klattese_plain = paste(klattese_plain, phoneme, sep = "")
+    }
+  }
+  return(klattese_plain)
+}
+
+# phoneme categories 
+engl_voiceless_cons <- c("C","f","h","k","p","s","S","t","T")
+engl_voiced_cons <- c("b","d","D","F","g","J","l","M","m","N","n","G","r","v","w","y","z","Z")  # word final M and N? 
+# engl_syll_cons <- c("L", "M", "N", "R") 
+engl_fricatives <- c("D","f","h","s","S","T","v","z","Z")
+engl_affricates <- c("C","J")
+engl_velars <- c("k","g","G")
+engl_liquids <- c("l","L","r","R","X")
 
 word_db <- read.csv('/Users/lindsaygreene/Desktop/programming/wcmRatio/UNCCombWordDB.csv', na.strings=c("", "NA"))
-data_path <- file.path("", "Users", "lindsaygreene", "Desktop")
+data_path <- file.path("", "Users", "lindsaygreene", "Desktop", "programming", "wcmRatio")
+
+# isolate the categories we need from word_db
+tibbletest <-tibble(word_db$phon_klattese, word_db$polysyll, word_db$nonInitialPrimaryStress, word_db$SUBTLWF0to10)   
 
 # set up data frame to store average results  
-data <- data.frame(matrix(vector(), ncol=4, nrow=length(files)))  # data frame to store avg output  
-files <- list.files(path=data_path, pattern="*.csv")
+data <- data.frame(matrix(vector(), ncol=3, nrow=length(files)))  # data frame to store avg output  
+files <- list.files(path=data_path, pattern="*practice.csv")
 header_names <- list("Avg_Target_Score","Avg_Actual_Score","Avg_WF_Score")  # column headers for avg output df 
 colnames(data) <- header_names
 rownames(data) <- files
@@ -21,7 +91,70 @@ for(file in 1:length(files)) {
   filePath <- paste(data_path, "/", fileName, sep="")  # update file name to absolute path 
   transcript <- read.csv(filePath, na.strings=c("", "NA"))  # read in csv and handle NA values 
   
+  # initialize vectors that will be populated with data for each word in sample 
+  foundInDB_tscript <- c()  # each target Klattese word that is found in the db
+  polysyll_tscript <- c()  # whether each word is polysyllabic
+  nonInitPrimStress_tscript <- c()  # whether each word has non-initial primary stress 
+  wf_tscript <- c()  # frequency of each word 
   
+  # initialize cumulative points for each file 
+  target_phon_total <- actual_phon_total <- wf_total <- 0 
   
+  # populate vectors with data for each word in the transcript
+  for(i in 1:nrow(transcript)) {
+    word <- toString(transcript[i,1])
+    row <- which(tibbletest[,1] == word)
+    if(!identical(toString(tibbletest[row, 1]),"character(0)")){  # omit words not found in word_db
+      foundInDB_tscript <- append(foundInDB_tscript, toString(tibbletest[row, 1]))
+      polysyll_tscript <- append(polysyll_tscript, toString(tibbletest[row, 2]))
+      nonInitPrimStress_tscript <- append(nonInitPrimStress_tscript, toString(tibbletest[row, 3]))
+      wf_tscript <- append(wf_tscript, toString(tibbletest[row, 4]))
+    }
+  }
+  
+  # transform vectors into data frames 
+  foundInDB_tscript<-as.data.frame(foundInDB_tscript)
+  polysyll_tscript<-as.data.frame(polysyll_tscript)
+  nonInitPrimStress_tscript<-as.data.frame(nonInitPrimStress_tscript)
+  wf_tscript<-as.data.frame(wf_tscript)
+  
+  for(word in 1:nrow(foundInDB_tscript)) {
+    target <- foundInDB_tscript[word,1]
+    actual <- transcript[which(transcript[,1] == target), 2]  # TODO: verify this line
+    target_plain <- removeMarkers(target)
+    actual_plain <- removeMarkers(actual)
+    target_wcm <- calculateWCM(target, word)
+    actual_wcm <- calculateWCM(actual, word)
+    wf <- as.double(wf_tscript[word,1])
+    
+    # calculate & store info in word by word output 
+    word_by_word[wbw_row, 1] = fileName
+    word_by_word[wbw_row, 2] = target_plain
+    word_by_word[wbw_row, 3] = actual_plain
+    word_by_word[wbw_row, 4] = target_wcm
+    word_by_word[wbw_row, 5] = actual_wcm
+    word_by_word[wbw_row, 6] = wf
+    
+    wbw_row = wbw_row + 1  # move to next row in the word by word df 
+    
+    # add points for current word to cumulative total 
+    target_phon_total = target_phon_total + target_wcm
+    actual_phon_total = actual_phon_total + actual_wcm
+    wf_total = wf_total + wf
+    
+  }
+  # calculate averages for file from total points 
+  avg_target_wcm <- target_phon_total/nrow(foundInDB_tscript)
+  avg_actual_wcm <- actual_phon_total/nrow(foundInDB_tscript)
+  avg_wf <- wf_total/nrow(wf_tscript)
+  
+  # write output and file name to avg output data frame  
+  data[file,1] = avg_target_wcm
+  data[file,2] = avg_actual_wcm
+  data[file,3] = avg_wf
 }
+
+# write output to file and save to same location to be opened in excel 
+write.csv(data, file=paste(data_path, "/", "wcmRatio_output.csv", sep=""))
+write.csv(word_by_word, file=paste(data_path, "/", "wcmRatio_word_by_word.csv", sep=""))
 
